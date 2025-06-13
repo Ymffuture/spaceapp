35d9c03b930e428b7f196db7c0d8c5a91a5dccc5
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-13 16:56:47 +0200

    Update Signup.jsx

--- kgclient/src/pages/Signup.jsx
+++ kgclient/src/pages/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
         console.log(user)
 
         try {
-            const response = await axios.post(`http://localhost:9123/api/v1/user/register`, user, {
+            const response = await axios.post(`https://kgserver-bjy2.onrender.com/api/v1/user/register`, user, {
                 headers: {
                     "Content-Type": "application/json",
                 },
