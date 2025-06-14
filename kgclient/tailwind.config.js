28f3f3995fa1c4c0a7151f5abe0163b047397fd0
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-14 09:45:54 +0200

    Update tailwind.config.js

--- kgclient/tailwind.config.js
+++ kgclient/tailwind.config.js
@@ -56,3 +56,13 @@ export default {
   plugins: [require("tailwindcss-animate")],
 }
 
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+}
+
