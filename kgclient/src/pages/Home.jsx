e7c515188fbbc3e27adb6c2c511e22fcfd9d6772
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-15 02:09:09 +0200

    Update Home.jsx

--- kgclient/src/pages/Home.jsx
+++ kgclient/src/pages/Home.jsx
@@ -2,14 +2,21 @@ import Hero from '@/components/Hero'
 import React from 'react'
 import RecentBlog from '@/components/RecentBlog'
 import PopularAuthors from '@/components/PopularAuthors'
-
+import {Helmet} from 'react-helmet' 
 const Home = () => {
   return (
+    <>
+    <Helmet>
+        <title>Home | Qspace</title>
+        <meta name="description" content="Read our latest tech, coding, and career articles." />
+      </Helmet>
+    
     <div className='pt-20'>
       <Hero/>
       <RecentBlog/>
       <PopularAuthors/>
     </div>
+    </>
   )
 }
 
