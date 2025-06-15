e11dc7e307a1ddc6fa7380bc26012087c31cbf9d
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-15 07:44:58 +0200

    Update Dashboard.jsx

--- kgclient/src/pages/Dashboard.jsx
+++ kgclient/src/pages/Dashboard.jsx
@@ -1,15 +1,21 @@
 import Sidebar from '@/components/Sidebar'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import {Helmet} from 'react-helmet';
 const Dashboard = () => {
     return (
+<>
+        <Helmet>
+        <title>Dashboard | Qspace</title>
+        <meta name="description" content="Read our latest tech, coding, and career articles." />
+      </Helmet>
         <div className=' flex'>
             <Sidebar />
             <div className='flex-1'>
                 <Outlet />
             </div>
         </div>
+</>
     )
 }
 
