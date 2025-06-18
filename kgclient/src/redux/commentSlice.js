847b97b8258ce6426d640de53c2b65cb2287e98a
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-18 03:04:28 +0200

    Update commentSlice.js

--- kgclient/src/redux/commentSlice.js
+++ kgclient/src/redux/commentSlice.js
@@ -1,30 +1,21 @@
-// src/redux/commentSlice.js
+// And how about my commentslice
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  loading: false,
-  comment: [],
-};
-
 const commentSlice = createSlice({
-  name: "comment",
-  initialState,
-  reducers: {
-    setLoading: (state, action) => {
-      state.loading = action.payload;
+    name:"comment",
+    initialState:{
+        loading:false,
+        comment:"",
     },
-    setComment: (state, action) => {
-      state.comment = Array.isArray(action.payload) ? action.payload : [];
-    },
-    addComment: (state, action) => {
-      if (!Array.isArray(state.comment)) {
-        state.comment = [];
-      }
-      state.comment.push(action.payload);
-    },
-  },
+    reducers:{
+        //actions
+        setLoading:(state, action) => {
+            state.loading = action.payload;
+        },
+        setComment:(state, action) => {
+            state.comment = action.payload;
+        }  
+    }
 });
-
-export const { setLoading, setComment, addComment } = commentSlice.actions;
+export const {setLoading, setComment} = commentSlice.actions;
 export default commentSlice.reducer;
-
