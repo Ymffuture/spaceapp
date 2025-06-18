4f88b19c8a8800bd8be033e0b0b784c91c36f469
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-18 03:01:28 +0200

    Update Store.js

--- kgclient/src/redux/Store.js
+++ kgclient/src/redux/Store.js
@@ -1,8 +1,22 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+// How about store 
+// import { configureStore } from '@reduxjs/toolkit'
+// import authSlice from "./authSlice"
+
+// const store = configureStore({
+//   reducer: {
+//    auth:authSlice
+//   },
+// })
+
+// export default store
+
+
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import blogSlice from "./blogSlice";
-import themeSlice from "./themeSlice";
-import commentSlice from "./commentSlice";
+import themeSlice from "./themeSlice"
+import commentSlice from "./commentSlice"
 
 import {
   persistReducer,
@@ -12,36 +26,32 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+// import companySlice from "./companySlice";
+// import applicationSlice from "./applicationSlice";
 
 const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-};
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  const rootReducer = combineReducers({
+    auth:authSlice,
+    blog:blogSlice,
+    comment:commentSlice,
+    theme: themeSlice,
+  })
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const rootReducer = combineReducers({
-  auth: authSlice,
-  blog: blogSlice,
-  comment: commentSlice,
-  theme: themeSlice,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
-        ],
-      },
-    }),
-  devTools: process.env.NODE_ENV !== 'production',
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
 });
-
 export default store;
-
