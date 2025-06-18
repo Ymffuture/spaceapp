ebca48492c89a7b88ddd349a6fd3e2ec384db112
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-18 03:02:31 +0200

    Update authSlice.js

--- kgclient/src/redux/authSlice.js
+++ kgclient/src/redux/authSlice.js
@@ -1,34 +1,26 @@
+
+// How about my authslice
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUser = JSON.parse(localStorage.getItem("user")) || null;
-
 const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    loading: false,
-    user: initialUser,
-    userProfile: null,
-  },
-  reducers: {
-    setLoading: (state, action) => {
-      state.loading = action.payload;
+    name:"auth",
+    initialState:{
+        loading:false,
+        user:null,
+        userProfile:null
     },
-    setUser: (state, action) => {
-      state.user = action.payload;
-      localStorage.setItem("user", JSON.stringify(action.payload));
-    },
-    setUserProfile: (state, action) => {
-      state.userProfile = action.payload;
-    },
-    logout: (state) => {
-      state.user = null;
-      state.userProfile = null;
-      state.loading = false;
-      localStorage.removeItem("user");
+    reducers:{
+        //actions
+        setLoading:(state, action) => {
+            state.loading = action.payload;
+        },
+        setUser:(state, action) => {
+            state.user = action.payload;
+        },
+        setUserProfile:(state, action) => {
+            state.userProfile = action.payload;
+        }
     }
-  },
 });
-
-export const { setLoading, setUser, setUserProfile, logout } = authSlice.actions;
+export const {setLoading, setUser, setUserProfile} = authSlice.actions;
 export default authSlice.reducer;
-
