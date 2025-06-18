7d56a0a9069406eb5bf274f713e7bd8eb30b75f3
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-18 03:03:27 +0200

    Update themeSlice.js

--- kgclient/src/redux/themeSlice.js
+++ kgclient/src/redux/themeSlice.js
@@ -1,34 +1,19 @@
+// How about themeslice
 import { createSlice } from "@reduxjs/toolkit";
 
-const getInitialTheme = () => {
-  // 1. If saved in localStorage, use it
-  const stored = localStorage.getItem("theme");
-  if (stored) return stored;
-
-  // 2. Else, use system preference
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  return prefersDark ? "dark" : "light";
-};
-
 const initialState = {
-  theme: getInitialTheme(),
-};
+    theme: 'light',
+}
 
 const themeSlice = createSlice({
-  name: "theme",
-  initialState,
-  reducers: {
-    setTheme: (state, action) => {
-      state.theme = action.payload;
-      localStorage.setItem("theme", action.payload);
-    },
-    toggleTheme: (state) => {
-      state.theme = state.theme === "light" ? "dark" : "light";
-      localStorage.setItem("theme", state.theme);
-    },
-  },
-});
+    name : "theme",
+    initialState,
+    reducers: {
+        toggleTheme: (state) => {
+            state.theme = state.theme === 'light' ? 'dark' : 'light';
+        }
+    }
+})
 
-export const { toggleTheme, setTheme } = themeSlice.actions;
+export const {toggleTheme} = themeSlice.actions;
 export default themeSlice.reducer;
-
